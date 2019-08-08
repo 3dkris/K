3eae9c49699ee950db6d8987119fe31604afcb40
K <3dkris@gmail.com>
2019-08-09 08:33:45 +1200

    update

---
+++ path_map_linux.py
@@ -0,0 +1,7 @@
+{
+    'wing_pro_7/scripts': '~/.wingide6/scripts',
+    'wing_pro_7/snippets': '~/.wingide6/snippets',
+    'wing_pro_7/preferences': '~/.wingide6/preferences',
+    'wing_pro_7/perspectives': '~/.wingide6/perspectives',
+}
+
